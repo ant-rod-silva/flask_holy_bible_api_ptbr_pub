b9403cbcef668d70f86bdd1fc3400a89beeebce3
Antonio Rodrigo dos Santos Silva <20875150+rodrigosantosbr@users.noreply.github.com>
2018-09-18 16:27:32 -0300

    Add files via upload

--- jsonLivro.py
+++ jsonLivro.py
@@ -7,6 +7,262 @@ tb_livro = [
     {'id': 2,
      'id_testamento': 1,
      'livro': u'Êxodo'
+    },
+    {'id': 3,
+     'id_testamento': 1,
+     'livro': u'Levítico'
+    },
+    {'id': 4,
+     'id_testamento': 1,
+     'livro': u'Números'
+    },
+    {'id': 5,
+     'id_testamento': 1,
+     'livro': u'Deuteronômio'
+    },
+    {'id': 6,
+     'id_testamento': 1,
+     'livro': u'Josué'
+    },
+    {'id': 7,
+     'id_testamento': 1,
+     'livro': u'Juízes'
+    },
+    {'id': 8,
+     'id_testamento': 1,
+     'livro': u'Rute'
+    },
+    {'id': 9,
+     'id_testamento': 1,
+     'livro': u'I Samuel'
+    },
+    {'id': 10,
+     'id_testamento': 1,
+     'livro': u'II Samuel'
+    },
+    {'id': 11,
+     'id_testamento': 1,
+     'livro': u'I Reis'
+    },
+    {'id': 12,
+     'id_testamento': 1,
+     'livro': u'II Reis'
+    },
+    {'id': 13,
+     'id_testamento': 1,
+     'livro': u'I Crônicas'
+    },
+    {'id': 14,
+     'id_testamento': 1,
+     'livro': u'II Crônicas'
+    },
+    {'id': 15,
+     'id_testamento': 1,
+     'livro': u'Esdras'
+    },
+    {'id': 16,
+     'id_testamento': 1,
+     'livro': u'Neemias'
+    },
+    {'id': 17,
+     'id_testamento': 1,
+     'livro': u'Ester'
+    },
+    {'id': 18,
+     'id_testamento': 1,
+     'livro': u'Jó'
+    },
+    {'id': 19,
+     'id_testamento': 1,
+     'livro': u'Salmos'
+    },
+    {'id': 20,
+     'id_testamento': 1,
+     'livro': u'Provérbios'
+    },
+    {'id': 21,
+     'id_testamento': 1,
+     'livro': u'Eclesiastes'
+    },
+    {'id': 22,
+     'id_testamento': 1,
+     'livro': u'Cantares'
+    },
+    {'id': 23,
+     'id_testamento': 1,
+     'livro': u'Isaías'
+    },
+    {'id': 24,
+     'id_testamento': 1,
+     'livro': u'Jeremias'
+    },
+    {'id': 25,
+     'id_testamento': 1,
+     'livro': u'Lamentações'
+    },
+    {'id': 26,
+     'id_testamento': 1,
+     'livro': u'Ezequiel'
+    },
+    {'id': 27,
+     'id_testamento': 1,
+     'livro': u'Daniel'
+    },
+    {'id': 28,
+     'id_testamento': 1,
+     'livro': u'Oséias'
+    },
+    {'id': 29,
+     'id_testamento': 1,
+     'livro': u'Joel'
+    },
+    {'id': 30,
+     'id_testamento': 1,
+     'livro': u'Amós'
+    },
+    {'id': 31,
+     'id_testamento': 1,
+     'livro': u'Obadias'
+    },
+    {'id': 32,
+     'id_testamento': 1,
+     'livro': u'Jonas'
+    },
+    {'id': 33,
+     'id_testamento': 1,
+     'livro': u'Miquéias'
+    },
+    {'id': 34,
+     'id_testamento': 1,
+     'livro': u'Naum'
+    },
+    {'id': 35,
+     'id_testamento': 1,
+     'livro': u'Habacuque'
+    },
+    {'id': 36,
+     'id_testamento': 1,
+     'livro': u'Sofonias'
+    },
+    {'id': 37,
+     'id_testamento': 1,
+     'livro': u'Ageu'
+    },
+    {'id': 38,
+     'id_testamento': 1,
+     'livro': u'Zacarias'
+    },
+    {'id': 39,
+     'id_testamento': 1,
+     'livro': u'Malaquias'
+    },
+    {'id': 40,
+     'id_testamento': 2,
+     'livro': u'Mateus'
+    },
+    {'id': 41,
+     'id_testamento': 2,
+     'livro': u'Marcos'
+    },
+    {'id': 42,
+     'id_testamento': 2,
+     'livro': u'Lucas'
+    },
+    {'id': 43,
+     'id_testamento': 2,
+     'livro': u'João'
+    },
+    {'id': 44,
+     'id_testamento': 2,
+     'livro': u'Atos'
+    },
+    {'id': 45,
+     'id_testamento': 2,
+     'livro': u'Romanos'
+    },
+    {'id': 46,
+     'id_testamento': 2,
+     'livro': u'I Coríntios'
+    },
+    {'id': 47,
+     'id_testamento': 2,
+     'livro': u'II Coríntios'
+    },
+    {'id': 48,
+     'id_testamento': 2,
+     'livro': u'Gálatas'
+    },
+    {'id': 49,
+     'id_testamento': 2,
+     'livro': u'Efésios'
+    },
+    {'id': 50,
+     'id_testamento': 2,
+     'livro': u'Filipenses'
+    },
+    {'id': 51,
+     'id_testamento': 2,
+     'livro': u'Colossenses'
+    },
+    {'id': 52,
+     'id_testamento': 2,
+     'livro': u'I Tessalonicenses'
+    },
+    {'id': 53,
+     'id_testamento': 2,
+     'livro': u'II Tessalonicenses'
+    },
+    {'id': 54,
+     'id_testamento': 2,
+     'livro': u'I Timóteo'
+    },
+    {'id': 55,
+     'id_testamento': 2,
+     'livro': u'II Timóteo'
+    },
+    {'id': 56,
+     'id_testamento': 2,
+     'livro': u'Tito'
+    },
+    {'id': 57,
+     'id_testamento': 2,
+     'livro': u'Filemon'
+    },
+    {'id': 58,
+     'id_testamento': 2,
+     'livro': u'Hebreus'
+    },
+    {'id': 59,
+     'id_testamento': 2,
+     'livro': u'Tiago'
+    },
+    {'id': 60,
+     'id_testamento': 2,
+     'livro': u'I Pedro'
+    },
+    {'id': 61,
+     'id_testamento': 2,
+     'livro': u'II Pedro'
+    },
+    {'id': 62,
+     'id_testamento': 2,
+     'livro': u'I João'
+    },
+    {'id': 63,
+     'id_testamento': 2,
+     'livro': u'II João'
+    },
+    {'id': 64,
+     'id_testamento': 2,
+     'livro': u'III João'
+    },
+    {'id': 65,
+     'id_testamento': 2,
+     'livro': u'Judas'
+    },
+    {'id': 66,
+     'id_testamento': 2,
+     'livro': u'Apocalipse'
     }
 ]
 
